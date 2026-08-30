f87a43d83a8b0cdb555cb88a921f765ab00aabdd
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-08-30 08:33:08 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int main() {
     string name;
-
     cout << "Enter your name: ";
     cin >> name;
-
-    cout << "Hello, " << name << "! Welcome to C++ programming." << std::endl;
-
+    cout << "Hello, " << name << "! Welcome to C++ programming." << endl;
     return 0;
 }
