d3648892aa912313eaeadfd7038387637a3a5c06
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-09-05 10:43:14 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main() {
     string name;
+    int age;
     cout << "Enter your name: ";
     cin >> name;
+    cout << "Enter your age: " ;
+    cin >> age;
     cout << "Hello, " << name << "! ";
+    cout << "Age, " << age;
     return 0;
 }
